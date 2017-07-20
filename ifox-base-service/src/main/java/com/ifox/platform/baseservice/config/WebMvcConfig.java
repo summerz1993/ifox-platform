17963d7d9457bbdca15b4115b8b701ddc0ffd55d
yezhang <summerzhang1413@gmail.com>
2017-07-20 16:44:56 +0800

    设置允许所有请求跨域访问

--- ifox-base-service/src/main/java/com/ifox/platform/baseservice/config/WebMvcConfig.java
+++ ifox-base-service/src/main/java/com/ifox/platform/baseservice/config/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package com.ifox.platform.baseservice.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -31,4 +32,14 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
 	}
 
+    /**
+     * 允许任何请求跨域访问
+     * @param registry CorsRegistry
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "HEAD", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "TRACE");
+    }
 }
