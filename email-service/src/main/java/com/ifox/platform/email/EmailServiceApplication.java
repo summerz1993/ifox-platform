b9a38d2d8c51d4448f208f49a3c77c5b1ae8235b
summer <summer_z1993@sina.com>
2017-08-30 23:34:08 +0800

    修改:email依赖base service

--- email-service/src/main/java/com/ifox/platform/email/EmailServiceApplication.java
+++ email-service/src/main/java/com/ifox/platform/email/EmailServiceApplication.java
@@ -1,17 +1,20 @@
 package com.ifox.platform.email;
 
+import com.ifox.platform.baseservice.config.PersistenceConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 /**
  * 邮件服务
  * @author Yeager
  */
 @SpringBootApplication
-@ComponentScan({"com.ifox.platform.email"})
+@ComponentScan(value = {"com.ifox.platform.email", "com.ifox.platform.baseservice.interceptor", "com.ifox.platform.baseservice.config"},
+    excludeFilters = {@ComponentScan.Filter(value = PersistenceConfig.class, type = FilterType.ASSIGNABLE_TYPE)})
 public class EmailServiceApplication {
 
     private static Logger logger = LoggerFactory.getLogger(EmailServiceApplication.class);
